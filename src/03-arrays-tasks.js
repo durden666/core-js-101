0dd526f0522759a79ed271633b246251cb1cd584
durden666 <thanks.vlad12@gmail.com>
2020-12-04 17:24:30 +0300

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,14 +35,15 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(len) {
-  const odds = [];
-  for (let i = 0; i < len * 2; i += 1) {
-    if (i % 2 !== 0) {
-      odds.push(i);
-    }
-  }
-  return odds;
+function generateOdds(/* len */) {
+  throw new Error('Not implemented');
+  // const odds = [];
+  // for (let i = 0; i < len * 2; i += 1) {
+  //   if (i % 2 !== 0) {
+  //     odds.push(i);
+  //   }
+  // }
+  // return odds;
 }
 
 
@@ -75,13 +76,7 @@ function doubleArray(arr) {
  *    [] => []
  */
 function getArrayOfPositives(arr) {
-  const positives = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] > 0) {
-      positives.push(arr[i]);
-    }
-  }
-  return positives;
+  return arr.filter((x) => x > 0);
 }
 
 /**
@@ -96,13 +91,7 @@ function getArrayOfPositives(arr) {
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
 function getArrayOfStrings(arr) {
-  const stringArray = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    if (typeof arr[i] === 'string') {
-      stringArray.push(arr[i]);
-    }
-  }
-  return stringArray;
+  return arr.filter((x) => typeof x === 'string');
 }
 
 /**
@@ -163,8 +152,8 @@ function getStringsLength(arr) {
  *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
@@ -192,8 +181,8 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(-n);
 }
 
 
@@ -232,8 +221,8 @@ function toCsvText(/* arr */) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  return arr.map((x) => x ** 2);
 }
 
 
@@ -372,13 +361,7 @@ function getItemsSum(arr) {
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
 function getFalsyValuesCount(arr) {
-  const farr = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] === false || arr[i] === null) {
-      farr.push(arr[i]);
-    }
-  }
-  return farr;
+  return arr.filter((x) => !!x === false).length;
 }
 
 /**
@@ -396,13 +379,7 @@ function getFalsyValuesCount(arr) {
  *    [ true, 0, 1, 'true' ], true => 1
  */
 function findAllOccurences(arr, item) {
-  let count = 0;
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] === item) {
-      count += 1;
-    }
-  }
-  return count;
+  return arr.filter((x) => x === item).length;
 }
 
 /**
@@ -486,8 +463,8 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array(end - start + 1).fill().map((_, i) => start + i);
 }
 
 /**
@@ -501,8 +478,9 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const setArr = new Set(arr);
+  return [...setArr];
 }
 
 /**
